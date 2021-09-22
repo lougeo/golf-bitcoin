906547d9108d99feb366c616df403b4978387b75
Louis George <louisg495@gmail.com>
2021-09-21 22:32:02 -0700

    add url files

--- golf/urls.py
+++ golf/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("course.urls", namespace="courses")),
+    path("", include("round.urls", namespace="rounds")),
+    path("", include("users.urls", namespace="users")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"))
 ]
