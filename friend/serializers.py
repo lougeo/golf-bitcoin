7c3334d9be6cfdab12cd4b63cc27b5a1cf7ac83f
Louis George <louisg495@gmail.com>
2022-01-06 01:09:28 -0800

    friend request viewset create

--- friend/serializers.py
+++ friend/serializers.py
@@ -2,10 +2,16 @@ from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
 
+from users.models import GolfUser
+
 from .models import Friendship, FriendRequest
 
 
 class FriendRequestSerializer(serializers.ModelSerializer):
+    sender = serializers.PrimaryKeyRelatedField(
+        required=False, queryset=GolfUser.objects.all()
+    )
+
     class Meta:
         model = FriendRequest
         fields = ["sender", "receiver", "accepted"]
--- friend/viewsets.py
+++ friend/viewsets.py
@@ -8,3 +8,7 @@ from .serializers import FriendRequestSerializer
 class FriendRequestViewSet(viewsets.ModelViewSet):
     queryset = FriendRequest.objects.all()
     serializer_class = FriendRequestSerializer
+
+    def create(self, request, *args, **kwargs):
+        request.data.update({"sender": request.user.pk})
+        return super().create(request, *args, **kwargs)
