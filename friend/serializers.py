6b7535283236fd75e559875a02468e1475a42563
Louis George <louisg495@gmail.com>
2022-01-08 14:54:49 -0800

    add route for my requests

--- friend/serializers.py
+++ friend/serializers.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 from users.models import GolfUser
+from users.serializers import UserSerializer
 
 from .models import Friendship, FriendRequest
 
@@ -11,7 +12,8 @@ class FriendRequestSerializer(serializers.ModelSerializer):
     sender = serializers.PrimaryKeyRelatedField(
         required=False, queryset=GolfUser.objects.all()
     )
+    sender_full = UserSerializer(read_only=True, source="sender")
 
     class Meta:
         model = FriendRequest
-        fields = ["sender", "receiver", "accepted"]
+        fields = ["id", "sender", "sender_full", "receiver", "accepted"]
--- friend/viewsets.py
+++ friend/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 from .models import Friendship, FriendRequest
@@ -8,6 +10,20 @@ from .serializers import FriendRequestSerializer
 class FriendRequestViewSet(viewsets.ModelViewSet):
     queryset = FriendRequest.objects.all()
     serializer_class = FriendRequestSerializer
+
+    @action(detail=False)
+    def mine(self, request):
+        queryset = self.filter_queryset(
+            FriendRequest.objects.filter(receiver=request.user, active=True)
+        )
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         request.data.update({"sender": request.user.pk})
